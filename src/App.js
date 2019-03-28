5debf45867dcb6d70574a46e0a3cc7300a1dad7b
azzaulil <azzaulil50@gmail.com>
2019-03-28 08:29:29 +0700

    second'

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      name : "Home",
+      color : "grey",
+      img : "home.jpg"
+    };
+  };
+
+  clicked(menu){
+    this.setState({
+      name : menu.name,
+      color : menu.color,
+      img : menu.img
+    });
+  }
+  render() {
+    return (
+	
+      <div id="app">
+      
+          <nav className="nav">{this.props.items.map((menu,index) => {
+            var style='menu';
+
+            if (this.state.color === menu.color){
+              style = `${style} is-active`;
+            }
+
+            return <a
+                  className={style+" "+menu.color}
+                  onClick={this.clicked.bind(this, menu)}
+                  key={index}>
+                  {menu.name}
+                </a>;
+          })}
+          </nav>
+
+          <div className="info">
+  ini adalah <span className={"selected "+this.state.color}>{this.state.name}</span>
+          <br></br>
+          <br></br>
+          <img src={this.state.img} width="240"></img>
+          </div>
+      </div>
+    
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Custom.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+
+const items = [
+    {
+        name : "Red",
+        color : "Merah",
+        img : "redberry.jpg"
+    },
+    {
+        name : "Yellow",
+        color : "Kuning",
+        img : "yellrose.jpg"
+    },
+    {
+        name : "Green",
+        color : "Hijau",
+        img : "green.jpg"
+    },
+    {
+        name : "Blue",
+        color : "Biru",
+        img : "blueberry.jpg"
+    }
+
+];
+
+
+ReactDOM.render(<App items={items}/>, document.getElementById('root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
+serviceWorker.register();
