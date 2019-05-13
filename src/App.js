65e687c978c6f637109a22452b9ef64ab2248e38
azzaulil <azzaulil50@gmail.com>
2019-05-13 14:03:34 +0700

    web

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+import Pr from './Profile.js';
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -9,48 +11,13 @@ class App extends Component {
     this.state = {
       name : "Home",
       color : "grey",
-      img : "home.jpg"
+      img : "home.jpg",
+      nilai1 : null,
+      nilai2 : null,
+      operator : null,
+      hasil : null,
     };
-  };
 
-  clicked(menu){
-    this.setState({
-      name : menu.name,
-      color : menu.color,
-      img : menu.img
-    });
   }
-  render() {
-    return (
-	
-      <div id="app">
-      
-          <nav className="nav">{this.props.items.map((menu,index) => {
-            var style='menu';
-
-            if (this.state.color === menu.color){
-              style = `${style} is-active`;
-            }
-
-            return <a
-                  className={style+" "+menu.color}
-                  onClick={this.clicked.bind(this, menu)}
-                  key={index}>
-                  {menu.name}
-                </a>;
-          })}
-          </nav>
-
-          <div className="info">
-  ini adalah <span className={"selected "+this.state.color}>{this.state.name}</span>
-          <br></br>
-          <br></br>
-          <img src={this.state.img} width="240"></img>
-          </div>
-      </div>
-    
-    );
-  }
-}
 
 export default App;
